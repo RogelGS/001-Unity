5c923a22aa39749d5b0c8603a7a44eecbd578167
RogelGS <rogel.gnz.snz@gmail.com>
2021-02-09 12:43:44 -0600

    primera pratica

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public static int coinsCount = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        Coin.coinsCount++;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider){
+        if(collider.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy() {
+        Coin.coinsCount--;
+        if (Coin.coinsCount <= 0) {
+            GameObject timer = GameObject.Find("Timer");
+            Destroy(timer);
+
+            GameObject[] fireworks = GameObject.FindGameObjectsWithTag("Firework");
+            foreach(GameObject firework in fireworks) {
+                firework.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float maxTime = 60.0f;
+    private float countDown = 0.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        countDown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0) {
+            Coin.coinsCount = 0;
+            SceneManager.LoadScene("Level001");
+        }
+    }
+}
